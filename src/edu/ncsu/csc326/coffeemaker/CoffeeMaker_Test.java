ac18144bf5be53059fdc86cc88c83034b73df429
Christian <bluehosebone311@yahoo.com>
2016-04-02 12:50:48 -0400

    Initial: Clean source code

---
+++ src/edu/ncsu/csc326/coffeemaker/CoffeeMaker_Test.java
@@ -0,0 +1,22 @@
+package edu.ncsu.csc326.coffeemaker;
+
+import junit.framework.TestCase;
+
+public class CoffeeMaker_Test extends TestCase
+{
+
+	public CoffeeMaker_Test(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception
+	{
+		super.tearDown();
+	}
+
+}
