5e6d6933687d7570acde9bff4c1d35176c6db44a
Christian <bluehosebone311@yahoo.com>
2016-04-04 18:44:58 -0400

    AddedTestSuite for running all JUnit Tests

---
+++ unittests/edu/ncsu/csc326/coffeemaker/TestSuite_JUint.java
@@ -0,0 +1,15 @@
+package edu.ncsu.csc326.coffeemaker;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+   CoffeeMakerTest.class,
+   InventoryTest.class,
+   RecipeBook_Test.class,
+   RecipeTest.class,
+})
+public class TestSuite_JUint
+{
+	
+}
